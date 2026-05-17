79238edc8506d15c662a240bf7153e7b1e33e157
maxmesquitashima <max.shimabucuro@gmail.com>
2026-05-17 00:59:04 -0300

    feat: implementa calculo de compatibilidade

--- skillmatch.js
+++ skillmatch.js
@@ -26,3 +26,44 @@ const listaVagas = [
   { empresa: "CodeLab", cargo: "Estagiário Front-End", requisitos: ["HTML", "CSS", "JavaScript", "Lógica de Programação", "GitHub"], salario: 2000.0, modalidade: "Híbrido", nivel: "Estágio" },
   { empresa: "WebSolutions", cargo: "Desenvolvedor Front-End Júnior", requisitos: ["JavaScript", "React", "Redux", "TypeScript", "Git", "Metodologias Ágeis", "Comunicação"], salario: 4000.0, modalidade: "Presencial", nivel: "Júnior" }
 ];
+// [RF03, RF04, RF05, RF08] MOTOR DE LOGICA DE COMPATIBILIDADE
+function analisarVagasManualmente() {
+  console.log("\n--- Análise de Compatibilidade ---");
+  let melhorCompatibilidade = { percentual: -1, vaga: null };
+  let todasHabilidadesFaltantes = new Set(); 
+
+  for (const vaga of listaVagas) {
+    // [RF08] Método de Array: .filter() para encontrar o que o candidato atende
+    const habilidadesAtendidas = candidato.habilidades.filter((hab) =>
+      vaga.requisitos.includes(hab)
+    );
+
+    const totalRequisitos = vaga.requisitos.length;
+    const requisitosAtendidos = habilidadesAtendidas.length;
+
+    // [RF03] Cálculo matemático da percentagem
+    const percentualCompatibilidade = totalRequisitos > 0 ? (requisitosAtendidos / totalRequisitos) * 100 : 0;
+
+    // [RF04] Estrutura Condicional para Classificar o Fit
+    let classificacao;
+    if (percentualCompatibilidade >= 80) {
+      classificacao = "Alta compatibilidade";
+    } else if (percentualCompatibilidade >= 50) {
+      classificacao = "Média compatibilidade";
+    } else {
+      classificacao = "Baixa compatibilidade";
+    }
+
+    // [RF05] Listar Habilidades Faltantes
+    const habilidadesFaltantes = vaga.requisitos.filter(
+      (req) => !candidato.habilidades.includes(req)
+    );
+
+    habilidadesFaltantes.forEach((hab) => todasHabilidadesFaltantes.add(hab));
+
+    // [RF06] Encontrar Maior Compatibilidade
+    if (percentualCompatibilidade > melhorCompatibilidade.percentual) {
+      melhorCompatibilidade = { percentual: percentualCompatibilidade, vaga: vaga };
+    }
+  }
+}
